6a10e2a96742ede7d1af19ca96068682a9564c53
tkersten09 <tkersten09@gmail.com>
2021-07-22 12:04:46 +0200

    Converted AudioVideoFragment.kt to a data binding layout.
    And added an onClickListener to the playAudioButton and tested it with a Toast.

--- app/src/main/java/com/example/android/navigation/AudioVideoFragment.kt
+++ app/src/main/java/com/example/android/navigation/AudioVideoFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentAudioVideoBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +20,16 @@ class AudioVideoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_video, container, false)
+        val binding = DataBindingUtil.inflate<FragmentAudioVideoBinding>(
+            inflater, R.layout.fragment_audio_video, container, false
+        )
+
+        // Setup a onCLick Listener for playAudioButton
+        binding.playAudioButton.setOnClickListener {
+            Toast.makeText(context, "Play Audio", Toast.LENGTH_LONG).show()
+        }
+
+        return binding.root
     }
 
 }
