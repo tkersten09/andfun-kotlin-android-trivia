95bcdb654b85150dba32158c4400b0db8e25ef64
tkersten09 <tkersten09@gmail.com>
2021-07-23 13:28:09 +0200

    Added Sound to the playAudioButton
    The Button now toggles a sound on or off, when clicked.

--- app/src/main/java/com/example/android/navigation/AudioVideoFragment.kt
+++ app/src/main/java/com/example/android/navigation/AudioVideoFragment.kt
@@ -1,11 +1,14 @@
 package com.example.android.navigation
 
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.FragmentAudioVideoBinding
 
@@ -16,6 +19,12 @@ import com.example.android.navigation.databinding.FragmentAudioVideoBinding
  */
 class AudioVideoFragment : Fragment() {
 
+    lateinit var soundPool: SoundPool
+    var applauseID = 0
+    var silenceID = 0
+    var streamID = 0
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -24,9 +33,29 @@ class AudioVideoFragment : Fragment() {
             inflater, R.layout.fragment_audio_video, container, false
         )
 
+        val attributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        soundPool = SoundPool.Builder().setAudioAttributes(attributes).setMaxStreams(5).build()
+        soundPool.setOnLoadCompleteListener { soundPool: SoundPool, i: Int, i1: Int ->
+            this.soundPool.play(silenceID, 1f, 1f, 1, 0, 1.0f)
+        }
+
+        applauseID = soundPool.load(context, R.raw.applause, 1)
+        silenceID = soundPool.load(context, R.raw.silence, 1)
+
         // Setup a onCLick Listener for playAudioButton
         binding.playAudioButton.setOnClickListener {
-            Toast.makeText(context, "Play Audio", Toast.LENGTH_LONG).show()
+//            Toast.makeText(context, "Play Audio", Toast.LENGTH_LONG).show()
+
+            // Toggle Audio on or off
+            if(streamID != 0) {
+                soundPool.stop(streamID)
+                streamID = 0
+            } else {
+                streamID = soundPool.play(applauseID, 1f, 1f, 1, 0, 1.0f)
+            }
         }
 
         return binding.root
